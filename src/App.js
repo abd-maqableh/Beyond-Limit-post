d455c9a5b3087691935317bcb36b31e8b58cbe7a
abd.maqableh <abd.maqableh@gmail.com>
2023-12-28 17:50:38 +0300

    fetch users and show posts based on user selected

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Post from "./components/Post";
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Layout />} />
+            <Route path="/posts/:userId" element={<Post />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
